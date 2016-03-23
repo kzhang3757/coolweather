2cf166726a3786b49e7d9b39f1364e4fa5daf04e
kzhang <kzhangvip@163.com>
2016-03-23 10:59:56 +0800

    新增数据库帮助类，以及各表对应的实体类。

--- app/src/main/java/com/kzhang/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/kzhang/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kzhang.coolweather;
+package com.kzhang.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kzhang.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/kzhang/coolweather/model/Province.java
@@ -0,0 +1,29 @@
+package com.kzhang.coolweather.model;
+
+/**
+ * Created by ZK on 2016/3/23.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = this.id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+}
